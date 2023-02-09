b25ef2ab1765cddb6b62548dd1ca2437f5e1a6b9
fotomain <foto888999@gmail.com>
2023-02-09 19:44:41 +0200

    a

--- src/context_globals_todo/reducer.ts
+++ src/context_globals_todo/reducer.ts
@@ -3,58 +3,59 @@
 
 import { ITodosActions, TGloabal_props } from "./globals_types";
 export default function todosReducer(
-    state: TGloabal_props,
-    action: ITodosActions
+    now_props: TGloabal_props,
+    activity: ITodosActions
 ): TGloabal_props {
-    switch (action.type) {
+    switch (activity.type) {
         case "SETTER_USER": {
-            console.log(action.global_new_data)
+            console.log("SETTER_USER")
+            console.log(activity.global_new_data)
             return {
-                ...state,
-                current_user: action.global_new_data.user
+                ...now_props,
+                current_user: activity.global_new_data.user
             };
         }
 
         case "TOGGLE_TODO": {
-            const toggledTodo = state.todos.map(item => {
-                return item.id === action.global_new_data.id
-                    ? { ...action.global_new_data,
-                        completed: !action.global_new_data.completed,
-                        is_active: !action.global_new_data.is_active,
+            const toggledTodo = now_props.todos.map(item => {
+                return item.id === activity.global_new_data.id
+                    ? { ...activity.global_new_data,
+                        completed: !activity.global_new_data.completed,
+                        is_active: !activity.global_new_data.is_active,
                         }
                     : item;
             });
             return {
-                ...state,
+                ...now_props,
                 todos: toggledTodo,
             };
         }
         case "DELETE_TODO": {
-            const deletedTodo = state.todos.filter(item => {
-                return item.id !== action.global_new_data.id;
+            const deletedTodo = now_props.todos.filter(item => {
+                return item.id !== activity.global_new_data.id;
             });
             return {
-                ...state,
+                ...now_props,
                 todos: deletedTodo
             };
         }
         case "ADD_TODO": {
             const newTodo = {
-                id: action.global_new_data.id,
-                text: action.global_new_data.text,
-                completed: action.global_new_data.completed,
-                is_active: action.global_new_data.is_active,
+                id: activity.global_new_data.id,
+                text: activity.global_new_data.text,
+                completed: activity.global_new_data.completed,
+                is_active: activity.global_new_data.is_active,
             };
 
             console.log('=== newTodo',newTodo)
 
             return {
-                ...state,
-                todos: [...state.todos, newTodo]
+                ...now_props,
+                todos: [...now_props.todos, newTodo]
             };
         }
         default: {
-            return state;
+            return now_props;
         }
     }
 }
